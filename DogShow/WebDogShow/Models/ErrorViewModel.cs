5243d1bff34147334026daafed94491ef6796fcf
Litovetc Andrii <litovetc2@gmail.com>
2020-03-03 13:34:39 +0200

    changed project to core 3.1

---
+++ DogShow/WebDogShow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebDogShow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
